d68b52c814689a85e75f15d07214c4fd0566420e
Shohan-CtRl <66548046+Shohan-CtRl@users.noreply.github.com>
2021-05-25 19:59:26 +0530

    Updates

---
+++ precache-manifest.3e5091de420e0733c7fac6a76a21d42f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5a03400bedc85987885f3c439ecda9",
+    "url": "/ShohanSaverimuttu/index.html"
+  },
+  {
+    "revision": "cbcc6e8cf4297640c4e3",
+    "url": "/ShohanSaverimuttu/static/css/2.f6122f81.chunk.css"
+  },
+  {
+    "revision": "e38fe5c8f26cda652781",
+    "url": "/ShohanSaverimuttu/static/css/main.f4f0d9c2.chunk.css"
+  },
+  {
+    "revision": "cbcc6e8cf4297640c4e3",
+    "url": "/ShohanSaverimuttu/static/js/2.06a88891.chunk.js"
+  },
+  {
+    "revision": "8db2ae7ab4d8b47464b2fceb420c9eb7",
+    "url": "/ShohanSaverimuttu/static/js/2.06a88891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38fe5c8f26cda652781",
+    "url": "/ShohanSaverimuttu/static/js/main.8fb7785f.chunk.js"
+  },
+  {
+    "revision": "9085eb29f31c8382c702",
+    "url": "/ShohanSaverimuttu/static/js/runtime-main.7ea3b8eb.js"
+  },
+  {
+    "revision": "c29b0b23aafd774b0f9e9797a73a40fa",
+    "url": "/ShohanSaverimuttu/static/media/GateIn.c29b0b23.png"
+  },
+  {
+    "revision": "ccb2a760af08bafac54c68a174d05fe3",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_Chat.ccb2a760.jpeg"
+  },
+  {
+    "revision": "01c104ca54d33bcc3f0405acb447c9fe",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_EmotionChart.01c104ca.jpeg"
+  },
+  {
+    "revision": "e604d47fbf4ad0c59faabe4e150fa12d",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_Home.e604d47f.jpeg"
+  },
+  {
+    "revision": "e0bea5e488885d871e7aac76303a2fcf",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_Logo.e0bea5e4.jpeg"
+  },
+  {
+    "revision": "7edd3c42e8eb1a1afe8a691f4bc948af",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_MusicRec.7edd3c42.jpeg"
+  },
+  {
+    "revision": "10c477140fdd8306219bc80aebbe12b5",
+    "url": "/ShohanSaverimuttu/static/media/PIKAIA_Setting.10c47714.jpeg"
+  },
+  {
+    "revision": "1249dda5a784587e8a8062baa313e4a5",
+    "url": "/ShohanSaverimuttu/static/media/Placement Agreement_Shohan Saverimuttu_20191184_CS.1249dda5.pdf"
+  },
+  {
+    "revision": "7b8f961401ac2dd40fc8c3fe163ebf4c",
+    "url": "/ShohanSaverimuttu/static/media/Shohan.7b8f9614.png"
+  },
+  {
+    "revision": "b289400cf6036fb4f0f1349bf37ab5c1",
+    "url": "/ShohanSaverimuttu/static/media/comingSoon.b289400c.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/ShohanSaverimuttu/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ShohanSaverimuttu/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/ShohanSaverimuttu/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ShohanSaverimuttu/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ShohanSaverimuttu/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ShohanSaverimuttu/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/ShohanSaverimuttu/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/ShohanSaverimuttu/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/ShohanSaverimuttu/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/ShohanSaverimuttu/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/ShohanSaverimuttu/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/ShohanSaverimuttu/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/ShohanSaverimuttu/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/ShohanSaverimuttu/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/ShohanSaverimuttu/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/ShohanSaverimuttu/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/ShohanSaverimuttu/static/media/myImage.bd80794f.png"
+  }
+]);
